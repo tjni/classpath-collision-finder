839a69d89ab821a8aed8d920ce6484757df9e53e
Theodore Ni <tni@linkedin.com>
2019-05-08 14:12:28 -0700

    Setup basic plugin structure.

---
+++ src/main/java/com/example/classpath/ClasspathCollisionFinderPlugin.java
@@ -0,0 +1,12 @@
+package com.example.classpath;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+
+public class ClasspathCollisionFinderPlugin implements Plugin<Project> {
+  @Override
+  public void apply(Project project) {
+    // TODO: Create a task that looks for classpath collisions.
+  }
+}
---
+++ src/main/java/com/example/classpath/tasks/ClasspathCollisionFinderTask.java
@@ -0,0 +1,13 @@
+package com.example.classpath.tasks;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+
+public class ClasspathCollisionFinderTask extends DefaultTask {
+
+  @TaskAction
+  public void findCollisions() {
+    // TODO: Implement the business logic.
+  }
+}
